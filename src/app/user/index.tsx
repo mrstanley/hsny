46281ba6cdca771011f968d4dfb5377838cb4a0d
xiaolin li <xiaolinli@xiaolindeMacBook-Pro.local>
2018-06-09 11:41:31 +0800

    创建消息、用户中心模块

---
+++ src/app/user/index.tsx
@@ -0,0 +1,33 @@
+declare const plus, mui, require;
+
+import { h, render, Component } from "preact";
+import Mixins from "../../components/base";
+import Utils from "../../utils";
+
+interface AppProps { }
+interface AppState { }
+
+export default class App extends Component<AppProps, AppState> {
+    mixins = [Mixins];
+    init: Function;
+    constructor(props: AppProps) {
+        super(props);
+        this.mixins.forEach(m => Object.assign(this, m));
+        this.init();
+    }
+    componentDidMount() {
+        Utils.setImmersed();
+    }
+    render(props: AppProps, state: AppState) {
+        return (
+            <div className="app-container">
+                <header id="header" class="mui-bar mui-bar-transparent">
+                    <a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left"></a>
+                    <h1 class="mui-title">导航栏</h1>
+                </header>
+            </div>
+        );
+    }
+}
+
+render(<App />, document.body);
