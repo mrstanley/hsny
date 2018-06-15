300caba4bac15bf4ba15efcabb8d90103b1b18f6
xiaolin li <xiaolinli@xiaolindeMacBook-Pro.local>
2018-06-15 15:59:47 +0800

    优化服务结构

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,68 +1,55 @@
 import { REMOTE } from '../config';
 import { fetch } from './http';
 
-let login = params => {
-    return fetch('post', `${REMOTE}/user/appLogin`, params);
-};
+// 登录
+let login = params => fetch('post', `${REMOTE}/user/appLogin`, params);
 
-let dict = params => {
-    return fetch('post', `${REMOTE}/dict/all`, params);
-};
+// 码表
+let dict = params => fetch('post', `${REMOTE}/dict/all`, params);
 
+// 主页
 let main = {
-    getCollectAreaList: params => {
-        return fetch('post', `${REMOTE}/collectArea/list/1/10`, params);
-    }
+    getCollectAreaList: params => fetch('post', `${REMOTE}/collectArea/list/1/10`, params)
 };
 
+// 消息
 let message = {
-    // /v1/msg/graphqlQuery
-    getMessage: params => {
-        return fetch('post', `${REMOTE}/msg/graphqlQuery`, params);
-    }
+    getMessage: params => fetch('post', `${REMOTE}/msg/graphqlQuery`, params)
 }
 
 // 业务数据
 let serviceData = {
-    serviceDataAdd: params => {
-        return fetch('post', `${REMOTE}/serviceData/add`, params);
-    },
+    serviceDataAdd: params => fetch('post', `${REMOTE}/serviceData/add`, params),
     serviceDataList: params => {
         let page = params.page;
         delete params.page;
         return fetch('post', `${REMOTE}/serviceData/list/${page}/10`, params);
     },
-    // /v1/serviceData/serviceDataUpload 上传
-    serviceDataUpload: params => {
-        return fetch('get', `${REMOTE}/serviceData/serviceDataUpload?tableId=${params.tableId}`);
-    },
-    // /v1/serviceData/detailById 查询业务数据
-    serviceDataDetailById: params => {
-        return fetch('get', `${REMOTE}/serviceData/detailById?id=${params.id}`);
-    },
-    // /v1/serviceData/update
-    serviceDataUpdate: params => {
-        return fetch('post', `${REMOTE}/serviceData/update`, params);
-    }
+    // 上传
+    serviceDataUpload: params => fetch('get', `${REMOTE}/serviceData/serviceDataUpload?tableId=${params.tableId}`),
+    // 查询业务数据
+    serviceDataDetailById: params => fetch('get', `${REMOTE}/serviceData/detailById?id=${params.id}`),
+    // 更新
+    serviceDataUpdate: params => fetch('post', `${REMOTE}/serviceData/update`, params)
 };
+
 // 虚拟表数据
 let getTableData = {
-    updateStateOrUseState: params => {
-        return fetch('get', `${REMOTE}/virtualTable/updateStateOrUseState?state=${params.state}&tableId=${params.tableId}`);
-    },
+    updateStateOrUseState: params => fetch('get', `${REMOTE}/virtualTable/updateStateOrUseState?state=${params.state}&tableId=${params.tableId}`),
     getVirtualTableList: params => {
         let page = params.page;
         delete params.page;
         return fetch('post', `${REMOTE}/virtualTable/list/${page}/10`, params);
     },
-    getVirtualTableDetailById: params => {
-        return fetch('get', `${REMOTE}/virtualTable/detailById?id=${params.id}`);
-    },
-    // /v1/virtualTable/appPersonCenterStatictics
-    appPersonCenterStatictics: params => {
-        return fetch('get', `${REMOTE}/virtualTable/appPersonCenterStatictics`);
-    }
+    getVirtualTableDetailById: params => fetch('get', `${REMOTE}/virtualTable/detailById?id=${params.id}`),
+    appPersonCenterStatictics: params => fetch('get', `${REMOTE}/virtualTable/appPersonCenterStatictics`)
 };
+
+// 可视化统计图表(指标呈现)
+let visualStatistic = {
+    equipManage: params => fetch('get', `${REMOTE}/visualStatistic/equipManage?devAddr=${params.devAddr}`),
+    farmingManage: params => fetch('get', `${REMOTE}/visualStatistic/farmingManage?devAddr=${params.devAddr}`)
+}
 
 export default {
     dict,
@@ -70,5 +57,6 @@ export default {
     ...main,
     ...message,
     ...getTableData,
-    ...serviceData
+    ...serviceData,
+    ...visualStatistic
 }
